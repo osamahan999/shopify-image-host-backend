5e87cf55afa8e67d374634ca38663224ff13bb74
Osama Hanhan <hanhan.osama@gmail.com>
2020-12-21 14:46:42 -0800

    creating new repo built

---
+++ routes/repo.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+
+const xss = require('xss'); //used for cleaning user input
+const pool = require('../mysqlConnector'); //connection pool
+
+
+
+/**
+ * Creates a repo and gives user owner permissions
+ * 
+ * @param {String} userUUID
+ * @param {String} repoName
+ * @param {Boolean} publicRepo
+ */
+router.route('/newRepo').post((req, res) => {
+
+
+    //remove xss attack possibility
+    const cleanUserUUID = xss(req.body.userUUID);
+    const cleanRepoName = xss(req.body.repoName);
+    const cleanPublicRepo = xss(req.body.publicRepo);
+
+    if (cleanUserUUID.length < 1 || cleanRepoName.length < 1 || (cleanPublicRepo != 'true' && cleanPublicRepo != "false")) res.status(400).json("Invalid input");
+    else {
+        var public = false;
+        if (cleanPublicRepo == 'true') public = true;
+        //gets connection from pool
+        pool.getConnection((error, connection) => {
+            if (error) res.status(400).json('Error: ' + error);
+
+            connection.query("SELECT * FROM repository WHERE owner_id = (SELECT user_id FROM user WHERE userUUID = ?) AND name = ?", [cleanUserUUID, cleanRepoName],
+                (error, results, fields) => {
+                    if (error) res.status(400).json('Error: ' + error);
+                    else if (results.length > 0) {
+                        res.status(400).json("Error: this exact repo already exists!");
+                    } else {
+                        connection.query("INSERT INTO repository (owner_id, date_created, name, public)" +
+                            " VALUES ((SELECT user_id FROM user WHERE userUUID = ?), now(), ?, ? )",
+                            [cleanUserUUID, cleanRepoName, public], (error, results, fields) => {
+
+                                if (error) res.status(400).json('Error: ' + error);
+
+                                //if unique
+                                else {
+                                    connection.query("INSERT INTO user_repository_permissions " +
+                                        "(user_id, isOwner, canUpload, canDeleteImg, canRenameRepo, canDeleteRepo, repo_id)" +
+                                        " VALUES ((SELECT user_id FROM user WHERE userUUID = ?), 1, 1, 1, 1, 1, (SELECT repo_id FROM repository WHERE owner_id = " +
+                                        "(SELECT user_id FROM user WHERE userUUID = ?) AND name = ?))", [cleanUserUUID, cleanUserUUID, cleanRepoName], (error, results, fields) => {
+
+                                            if (error) res.status(400).json('Error: ' + error);
+                                            else res.json("Success");
+                                        })
+                                }
+                                connection.release();
+
+                            })
+                    }
+                })
+
+        })
+    }
+});
+
+
+
+module.exports = router
